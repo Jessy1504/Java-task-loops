402ad1075143ad86617f352660e589706c540024
Jessy1504 <Jessybonigala15@gmail.com>
2025-05-18 21:24:29 +0530

    Add files via upload

---
+++ ArmstrongNumbers.java
@@ -0,0 +1,26 @@
+package com.armstrongnumber;
+
+public class ArmstrongNumbers {
+	public static void main(String[] args) {
+		int number = 153;
+		int original = number;
+		int count = 0;
+		int sum = 0;
+		int temp = number;
+		while (temp > 0) {
+			count++;
+			temp /= 10;
+		}
+		temp = number;
+		while (temp > 0) {
+			int digit = temp % 10;
+			sum += Math.pow(digit, count);
+			temp /= 10;
+		}
+		if (sum == original) {
+			System.out.println(original + " is an Armstrong Number");
+		} else {
+			System.out.println(original + " is not an Armstrong Number");
+		}
+	}
+}
